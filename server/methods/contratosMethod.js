a4412ec64fa15fa32fe0c7c8d67a0cf6e50a27d6
*Falavigna Gustavo <gfalavigna@edesalsl.local>
2016-06-21 18:56:00 -0300

    con contratos andando

--- server/methods/contratosMethod.js
+++ server/methods/contratosMethod.js
@@ -19,7 +19,16 @@ Meteor.methods({
 			check(doc, ContratoSchema);
 			doc.fUM = new Date();
 			doc.usuario = this.userId;
-			var contrato = Contratos.update( {_id: doc._id }, {$set: {nombres: doc.nombres} });
+			var contrato = Contratos.update( {_id: doc._id }, {$set: 	{	nombres: doc.nombre, 
+																			clienteId: doc.clienteId,
+																			cuentaId: doc.cuentaId,
+																			tipoContrato: doc.tipoContrato,
+																			estadoContrato: doc.estadoContrato,
+																			dirPostalId: doc.dirPostalId,
+																			fAlta: doc.fAlta,
+																			fUM: doc.fUM,
+																			fBaja: doc.fBaja
+																		} });
 		}
 	}
 
